310185babdade2e907dcbcb003cfe3be61fcf513
xakyth <xakyth@gmail.com>
2024-03-05 06:23:46 +0300

    ex. 11.9 - updated cypress tests

---
+++ cypress/e2e/basic.cy.js
@@ -0,0 +1,16 @@
+describe('Pokedex', function () {
+  it('front page can be opened', function () {
+    cy.visit('http://localhost:5000');
+    cy.contains('ivysaur');
+    cy.contains(
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
+    );
+  });
+  it('pokemon page can be opened', function () {
+    cy.visit('http://localhost:5000');
+    cy.contains('ivysaur').click();
+    cy.contains('speed');
+    cy.contains('Hidden ability');
+    cy.contains('chlorophyll');
+  });
+});
