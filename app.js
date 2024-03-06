cdbf7302d921dc9d1b1bb3f7bcf0143a4f276dfe
xakyth <xakyth@gmail.com>
2024-03-06 07:12:16 +0300

    test commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static('dist'));
 
 app.get('/healthcheck', (req, res) => {
-  res.send('ok');
+  res.send('ok ok');
 });
 
 app.listen(PORT, () => {
